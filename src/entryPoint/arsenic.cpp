dc6559c4872f6ee9b7d429e52e8de16686d0c076
Kishan Parmar <kparmar2101@gmail.com>
2024-02-27 11:39:53 +0530

    Exit out of main checking ARGC

--- src/entryPoint/arsenic.cpp
+++ src/entryPoint/arsenic.cpp
@@ -9,10 +9,7 @@ errorReporting arsenicError;
 
 int arsenic_main(int argc, char *argv[]) {
 
-  if (argc > 2) {
-    std::cout << "Usage: arsenic [filename]" << std::endl;
-    exit(EXIT_FAILURE);
-  } else if (argc == 2) {
+  if (argc == 2) {
     runFile(argv[1]);
   } else {
     runPrompt();
@@ -23,6 +20,10 @@ int arsenic_main(int argc, char *argv[]) {
 } // namespace arsenic
 
 int main(int argc, char *argv[]) {
+  if (argc > 2) {
+    std::cout << "Usage: arsenic [filename]" << std::endl;
+    exit(EXIT_FAILURE);
+  }
   arsenic::arsenic_main(argc, argv);
   return 0;
 }
